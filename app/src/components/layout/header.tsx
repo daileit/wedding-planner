d1dd09475f1e9ec697a34318c6b4ce5a63e677e5
Dai Le <dai.le@lifestyleproject.io>
2026-02-06 17:54:27 +0700

    feat(language): header

--- app/src/components/layout/header.tsx
+++ app/src/components/layout/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useTransition } from "react";
+import React from "react";
 import { Bell, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
